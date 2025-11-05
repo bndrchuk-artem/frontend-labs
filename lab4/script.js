8804775d39df268a6636d7f8bfe8859dd622da9b
Artem Bondarchuk <artem.bondarchuk1233@gmail.com>
2025-11-05 19:05:48 +0200

    added color change

---
+++ lab4/script.js
@@ -0,0 +1,7 @@
+document.getElementById("date").addEventListener("click", (e) => {
+  e.currentTarget.classList.toggle("style1");
+});
+
+document.querySelector(".education").addEventListener("click", (e) => {
+  e.currentTarget.classList.toggle("style2");
+});
